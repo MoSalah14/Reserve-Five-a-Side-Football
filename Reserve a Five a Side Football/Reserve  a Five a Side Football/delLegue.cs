381fd69f1ce149d7de4e9a0db443e0178b85418b
gehadm2023 <152820581+gehadm2023@users.noreply.github.com>
2024-02-11 07:03:36 +0200

    delete reservation

--- Reserve a Five a Side Football/Reserve  a Five a Side Football/delLegue.cs
+++ Reserve a Five a Side Football/Reserve  a Five a Side Football/delLegue.cs
@@ -3,29 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace Reserve__a_Five_a_Side_Football
 {
-    public partial class delReservation : Form
+    public partial class delLegue : Form
     {
-      private readonly Reserve_a_Five_a_SideEntities context1;
-        public delReservation()
+        private readonly Reserve_a_Five_a_SideEntities context1;
+        public delLegue()
         {
             InitializeComponent();
             context1 = new Reserve_a_Five_a_SideEntities();
         }
-
-
-
-
-        
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -38,27 +31,31 @@ namespace Reserve__a_Five_a_Side_Football
             }
         }
 
-        private void del_Reservation_Load(object sender, EventArgs e)
+        private void delLegue_Load(object sender, EventArgs e)
         {
 
             var query = (from r in context1.Reservations
                          join p in context1.Players on r.Player_ID equals p.Player_ID
-                         join x in context1.Users on p.UserID equals 1
+                         join x in context1.Users on p.UserID equals x.UserID
+                         join z in context1.Legaues on r.StadiumID equals z.StadiumID
+
                          select new
                          {
                              FullName = x.FName + " " + x.LName,
-                             r.Reservation_Date,
-                             r.Reservation_Time,
-                             r.Payment,
-                             r.Reservation_Statues
+                             z.Legue_Name,
+                             z.StadiumName,
+                             z.Reward,
+                             z.BeginDate, 
+                             z.EndDate,
+                             z.TimePlay
+
+
                          });
 
             foreach (var item in query)
             {
-                dataGridView1.Rows.Add(item.FullName, item.Reservation_Date.Value.Date.ToString("yyyy-MM-dd"), item.Reservation_Time.ToString(), item.Payment,item.Reservation_Statues);
+                dataGridView1.Rows.Add(item.FullName,item.Legue_Name,item.StadiumName,item.BeginDate.Date.ToString("yyyy-MM-dd"), item.EndDate.Date.ToString("yyyy-MM-dd"), item.TimePlay, item.Reward);
             }
         }
     }
-
-    
 }
