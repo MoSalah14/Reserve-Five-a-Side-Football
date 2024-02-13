5830600c221927961a81d25b09bc47756b205f74
gehadm2023 <152820581+gehadm2023@users.noreply.github.com>
2024-02-13 07:49:42 +0200

    update&delete Legue

--- Reserve a Five a Side Football/Reserve  a Five a Side Football/delReservation.cs
+++ Reserve a Five a Side Football/Reserve  a Five a Side Football/delReservation.cs
@@ -6,8 +6,10 @@ using System.Data;
 using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
 
@@ -15,50 +17,55 @@ namespace Reserve__a_Five_a_Side_Football
 {
     public partial class delReservation : Form
     {
-      private readonly Reserve_a_Five_a_SideEntities context1;
+        private readonly Reserve_a_Five_a_SideEntities context1 = new Reserve_a_Five_a_SideEntities();
         public delReservation()
         {
             InitializeComponent();
-            context1 = new Reserve_a_Five_a_SideEntities();
+            show();
+
         }
-
-
-
-
-        
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+
+
             if (dataGridView1.SelectedRows.Count > 0)
             {
                 foreach (DataGridViewRow row in dataGridView1.SelectedRows)
                 {
-                    dataGridView1.Rows.Remove(row);
+
+                    int reservationId = int.Parse(dataGridView1.Rows[0].Cells["ReservationID"].Value.ToString());
+
+
+                    var q = context1.Reservations.FirstOrDefault(r => r.ReservationID == reservationId);
+
+
+                    context1.Reservations.Remove(q);
+                    context1.SaveChanges();
+                    show();
+
                 }
+
             }
         }
 
-        private void del_Reservation_Load(object sender, EventArgs e)
-        {
-
-            var query = (from r in context1.Reservations
+            private void show()
+            {
+                var y = (from r in context1.Reservations
                          join p in context1.Players on r.Player_ID equals p.Player_ID
                          join x in context1.Users on p.UserID equals x.UserID
                          select new
                          {
+                             r.ReservationID,
                              FullName = x.FName + " " + x.LName,
                              r.Reservation_Date,
                              r.Reservation_Time,
                              r.Payment,
                              r.Reservation_Statues
-                         });
-
-            foreach (var item in query)
-            {
-                dataGridView1.Rows.Add(item.FullName, item.Reservation_Date.Value.Date.ToString("yyyy-MM-dd"), item.Reservation_Time.ToString(), item.Payment,item.Reservation_Statues);
+                         }).ToList();
+                dataGridView1.DataSource = y;
             }
         }
-    }
 
-    
-}
+    }  
